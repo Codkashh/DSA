94045a65f69a7acae5ed6b217fb04367f2ad3ec4
Codkashh <as3733542@gmail.com>
2024-10-28 18:47:43 +0530

    missing number in array

---
+++ missing-number-in-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int N = nums.length;
+        int sum = N * (N + 1) / 2;
+        int sum2 = 0;
+        for(int i=0; i<N; i++){
+            sum2 += nums[i];
+        }
+        int missingNum = sum - sum2;
+        return missingNum;
+    }
+}
